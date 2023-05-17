e299b98770cf0162eb871419b1bae1a90ba53880
shvydak <y.shvydak@gmail.com>
2023-05-17 10:34:20 +0300

    RandomString

---
+++ src/main/java/org/example/RandomStringGenerator.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Random;
+
+public class RandomStringGenerator {
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) {
+        int length = 10; // Define the desired length of the random string
+
+        String randomString = generateRandomString(length);
+        System.out.println(randomString);
+    }
+
+    public static String generateRandomString(int length) {
+        StringBuilder sb = new StringBuilder();
+
+        // Generate the first letter in uppercase
+        sb.append(Character.toUpperCase(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length()))));
+
+        // Generate the remaining lowercase letters
+        for (int i = 1; i < length; i++) {
+            sb.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+
+        return sb.toString();
+    }
+}
